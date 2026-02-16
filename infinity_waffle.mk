6d3556bb538f68794e9dc82f006b4f68d23ca364
Jezzay97 <pincayjesse@gmail.com>
2026-02-15 20:34:35 -0500

    BringUp Project InfinityX

--- infinity_waffle.mk
+++ infinity_waffle.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from waffle device
 $(call inherit-product, device/oneplus/waffle/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_waffle
+PRODUCT_NAME := infinity_waffle
 PRODUCT_DEVICE := waffle
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -29,3 +29,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=CPH2573 \
     SystemDevice=OP595DL1 \
     SystemName=CPH2573
+
+# Maintainer Name
+INFINITY_MAINTAINER := "Jezzay097"
+
+# Whether the package includes System BLURS
+TARGET_SUPPORTS_BLUR := true
+
+# Whether the compiled package ships Google Apps:
+WITH_GAPPS := true
+
+# Whether the device supports Fingerprint On Display
+TARGET_HAS_UDFPS := true
